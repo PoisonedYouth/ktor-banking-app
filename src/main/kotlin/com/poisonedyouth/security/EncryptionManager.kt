da8f2f3a332136c63f00b41aca500677f708102a
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-09-16 12:30:33 +0200

    [V-12] Added EncryptionManager

---
+++ src/main/kotlin/com/poisonedyouth/security/EncryptionManager.kt
@@ -0,0 +1,55 @@
+package com.poisonedyouth.security
+
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.PBEKeySpec
+import javax.crypto.spec.SecretKeySpec
+import java.nio.charset.StandardCharsets
+import java.security.spec.KeySpec
+
+
+
+data class EncryptionResult(
+    val secretKey: ByteArray,
+    val initializationVector: ByteArray,
+    val ciphterText: ByteArray
+)
+
+private const val TRANSFORMATION = "AES/CBC/PKCS5PADDING"
+
+private const val ALGORITHM = "AES"
+
+object EncryptionManager {
+
+
+    fun encrypt(password: String): EncryptionResult {
+
+        val plainText = password.toByteArray(Charsets.UTF_8)
+        val keygen = KeyGenerator.getInstance(ALGORITHM)
+        keygen.init(256)
+
+        val key = keygen.generateKey()
+
+        val cipher = Cipher.getInstance(TRANSFORMATION)
+        cipher.init(Cipher.ENCRYPT_MODE, key)
+        val cipherText = cipher.doFinal(plainText)
+
+        return EncryptionResult(
+            secretKey = key.encoded,
+            initializationVector = cipher.iv,
+            ciphterText = cipherText
+        )
+    }
+
+    fun decrypt(encryptionResult: EncryptionResult): String {
+        val cipher = Cipher.getInstance(TRANSFORMATION)
+        val ivSpec = IvParameterSpec(encryptionResult.initializationVector)
+        cipher.init(Cipher.DECRYPT_MODE, SecretKeySpec(encryptionResult.secretKey, ALGORITHM), ivSpec)
+        val cipherText = cipher.doFinal(encryptionResult.ciphterText)
+
+        return String(cipherText, StandardCharsets.UTF_8)
+    }
+}
---
+++ src/test/kotlin/com/poisonedyouth/security/EncryptionManagerTest.kt
@@ -0,0 +1,33 @@
+package com.poisonedyouth.security
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class EncryptionManagerTest {
+
+    @Test
+    fun `encrypt and decrypt are working for correct data`() {
+        // given
+        val password = "password"
+
+        // when
+        val result = EncryptionManager.encrypt(password)
+
+        // then
+        assertThat(EncryptionManager.decrypt(result)).isEqualTo(password)
+    }
+
+    @Test
+    fun `encrypt and decrypt are working for incorrect data`() {
+        // given
+        val password = "password"
+
+        // when
+        val result = EncryptionManager.encrypt("otherPassword")
+
+        // then
+        assertThat(
+            EncryptionManager.decrypt(result)
+        ).isNotEqualTo(password)
+    }
+}
